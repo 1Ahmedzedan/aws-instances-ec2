d89bc4b32497816577e2dfb6a78e578bcf591c95
Ahmed Zedan <zedan@gmail.com>
2023-09-23 15:23:43 +0300

    add routering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Login from "./page/Login.js";
 import Signup from "./page/Signup.js";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
